a09ee5da334178714067d3d84424c79e338d2269
Bhuvi-01 <bhuvi251288@gmail.com>
2024-02-19 17:38:02 +0530

    input

--- src/com/rahul/Demo.java
+++ src/com/rahul/Demo.java
@@ -1,7 +1,14 @@
 package com.rahul;
 
+import java.util.Scanner;
+
 public class Demo {
     public static void main(String[] args){
-        System.out.println("Hello World!");
+//        System.out.print("Hello World!");
+//        System.out.println(243534);
+        Scanner Input=new Scanner(System.in);
+        System.out.println(Input.nextLine());
+
+
     }
 }
