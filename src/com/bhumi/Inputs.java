7fba6848cece48849a6910492a5b5edefae572c6
Bhuvi-01 <bhuvi251288@gmail.com>
2024-02-19 18:21:27 +0530

    sum and primitive

--- src/com/bhumi/Inputs.java
+++ src/com/bhumi/Inputs.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class Inputs {
     public static void main(String[] args){
-//        System.out.println("enter your rollno");
-//        Scanner input= new Scanner(System.in);
-//        int rollno=input.nextInt();
-//        System.out.println("your rollno is: " + rollno);
+       System.out.println("enter your rollno");
+         Scanner input= new Scanner(System.in);
+         int rollno=input.nextInt();
+       System.out.println("your rollno is: " + rollno);
         int a=234_000_000;
         System.out.println(a);
 
